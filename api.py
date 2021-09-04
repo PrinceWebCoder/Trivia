ddb44d3d4c76df7dc5e7c7da3b333f78a5ddb698
R. Coder <77580222+Esteklo@users.noreply.github.com>
2021-09-04 13:30:27 +0500

    Update api.py

--- api.py
+++ api.py
@@ -251,11 +251,14 @@ def create_app(test_config=None):
       'message': 'Sorry, the falut is us not you. Please try again later.'
     }), 500
   
-  if __name__=='__main__':
-    app.run()
   return app
 
-create_app()
+app = create_app()
+
+if __name__=='__main__':
+  app.run()
+
+
 
 
 # end of the code...
